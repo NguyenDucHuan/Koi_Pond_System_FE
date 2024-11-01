0a0723689e0b3fbfe33da58843a181e8cdf43714
NguyenDucHuan <NguyenDucHuan14012003@gmail.com>
2024-11-02 01:27:10 +0700

    Update EditBlog.jsx

--- src/pages/Admin/ManagerBlog/EditBlog.jsx
+++ src/pages/Admin/ManagerBlog/EditBlog.jsx
@@ -129,7 +129,7 @@ const EditBlog = () => {
           </label>
 
           <Editor
-            apiKey="rzelnlucawqxdndppr49miokeqs2tvdf8tjz1dg182z52ucv"
+            apiKey="9kv9bo3g9ip4unmc9p7bjk32rpgsbmcuqob3qv3s3qplwhug"
             value={content}
             init={{
               height: 500,
